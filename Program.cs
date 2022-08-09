7e8f0e4c102510aed0fb4b35d1f125bf6f10134d
Lucas <lucasgnclvs1@gmail.com>
2022-08-09 20:00:20 -0300

    Matrizes (identificacao de numeros negativos)

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a ordem da matriz: ");
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n, n];
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+
+            }
+
+            Console.WriteLine("Main value: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(mat[i, i] + " ");
+            }
+            Console.WriteLine("Negative numbers: ");
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Negative numbers: " + count);
+        }
+
+    }
+
+}
